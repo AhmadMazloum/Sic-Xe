51ede036982f42254de0cbc4efbc73b47b8682d2
Ahmad Mazloum <ahmadmazloum98@gmail.com>
2019-04-21 18:42:09 +0200

    start

---
+++ sic-xe/sic-xe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sic-xe
+//
+//  Created by Ahmad Mazloum on 4/21/19.
+//  Copyright © 2019 Ahmad Mazloum. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
