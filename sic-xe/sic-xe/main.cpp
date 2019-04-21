ce84163ae463afa86a55b2574fc451c7bf9274e4
Ahmad Mazloum <ahmadmazloum98@gmail.com>
2019-04-21 18:43:00 +0200

    push to all

--- sic-xe/sic-xe/main.cpp
+++ sic-xe/sic-xe/main.cpp
@@ -8,8 +8,8 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main(int argc, const char * argv[])
+{
     std::cout << "Hello, World!\n";
     return 0;
 }
