ded71d96ede42703180c5e23ef1afe60cb84204c
abdelrahmanfarid <38463761+abdelrahmanfarid@users.noreply.github.com>
2019-04-23 21:59:11 +0200

    Update main.cpp

--- sic-xe/sic-xe/main.cpp
+++ sic-xe/sic-xe/main.cpp
@@ -24,10 +24,117 @@ class instruction
     bool errors[14];
 };
 
+void check(instruction &n){
+    char arr[6]={'a','b','x','s','t','\0'}
+    bool found=false;
+ if(n.operation=="RMO"){
+        pc+=2;
+        n.memory=pc;
+       if(n.operand[1]!=','){n.errors[9]=true;}
+       for(int i=0;i<6;i++){
+        if(tolower(n.operand[0]==arr[i]){found=true;break;}
 
+       }
+       if(found==false){n.errors[9]=true;}
+       for(int i=0;i<6;i++){
+        if(tolower(n.operand[2]==arr[i]){found=true;break;}
+
+       }
+       if(found==false){n.errors[9]=true;}
+    }
+else if(!strcasecmp(n.operation,"+RMO")){
+        pc+=4;
+        n.memory=pc;
+       n.errors[11]=true;
+
+    }
+
+///
+
+ else if(!strcasecmp(n.operation,"ADDR")){
+        pc+=2;
+        n.memory=pc;
+       if(n.operand.at(1)!=','){n.errors[9]=true;}
+       for(int i=0;i<6;i++){
+        if(tolower(n.operand.at(0))==arr[i]){found=true;break;}
+
+       }
+       if(found==false){n.errors[9]=true;}
+       for(int i=0;i<6;i++){
+        if(tolower(n.operand.at(2))==arr[i]){found=true;break;}
+
+       }
+       if(found==false){n.errors[9]=true;}
+    }
+else if(!strcasecmp(n.operation,"+ADDR")){
+        pc+=4;
+        n.memory=pc;
+       n.errors[11]=true;
+
+    }
+///
+else if(!strcasecmp(n.operation,"SUBR")){
+        pc+=2;
+        n.memory=pc;
+       if(n.operand.at(1)!=','){n.errors[9]=true;}
+       for(int i=0;i<6;i++){
+        if(tolower(n.operand.at(0))==arr[i]){found=true;break;}
+
+       }
+       if(found==false){n.errors[9]=true;}
+       for(int i=0;i<6;i++){
+        if(tolower(n.operand.at(2))==arr[i]){found=true;break;}
+
+       }
+       if(found==false){n.errors[9]=true;}
+    }
+else if(!strcasecmp(n.operation,"+SUBR")){
+        pc+=4;
+        n.memory=pc;
+       n.errors[11]=true;
+
+    }
+///
+else if(!strcasecmp(n.operation,"COMPR")){
+        pc+=2;
+        n.memory=pc;
+       if(n->operand.at(1)!=','){n.errors[9]=true;}
+       for(int i=0;i<6;i++){
+        if(tolower(n.operand.at(0))==arr[i]){found=true;break;}
+
+       }
+       if(found==false){n.errors[9]=true;}
+       for(int i=0;i<6;i++){
+        if(tolower(n.operand.at(2))==arr[i]){found=true;break;}
+
+       }
+       if(found==false){n.errors[9]=true;}
+    }
+else if(!strcasecmp(n.operation,"+COMPR")){
+        pc+=4;
+        n.memory=pc;
+       n.errors[11]=true;
+
+    }
+///
+else if(!strcasecmp(n.operation,"TIXR")){
+        pc+=2;
+        n.memory=pc;
+        if()
+
+    }
+else if(!strcasecmp(n.operation,"+TIXR")){
+        pc+=4;
+        n.memory=pc;
+       n.errors[11]=true;
+
+    }
+///
+
+}
 int main(int argc, const char * argv[])
 {
-    std::cout << "Hello, World!\n";
+
     //
     return 0;
 }
