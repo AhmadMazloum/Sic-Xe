8622f8d1d0f0c0e5af6ae6fafc38534c77010609
abdelrahmanfarid <38463761+abdelrahmanfarid@users.noreply.github.com>
2019-04-23 22:13:14 +0200

    .

--- sic-xe/sic-xe/main.cpp
+++ sic-xe/sic-xe/main.cpp
@@ -14,14 +14,14 @@ using namespace std;
 int pc;
 class instruction
 {
-    int memory;
-    int line;
-    string operation;
-    string operand;
-    string label;
-    string comment;
-    string registers;
-    bool errors[14];
+  public:  int memory;
+  public:  int line;
+  public:  string operation;
+  public:  string operand;
+  public:  string label;
+  public:  string comment;
+  public:  string registers;
+  public:  bool errors[14];
     instruction()
     {
         for (int i = 0; i <= 14 ; i++)
@@ -32,24 +32,24 @@ class instruction
 };
 
 void check(instruction &n){
-    char arr[6]={'a','b','x','s','t','\0'}
+    char arr[6]={'a','b','x','s','t','\0'};
     bool found=false;
  if(n.operation=="RMO"){
         pc+=2;
         n.memory=pc;
        if(n.operand[1]!=','){n.errors[9]=true;}
        for(int i=0;i<6;i++){
-        if(tolower(n.operand[0]==arr[i]){found=true;break;}
+        if(tolower(n.operand[0])==arr[i]){found=true;break;}
 
        }
        if(found==false){n.errors[9]=true;}
        for(int i=0;i<6;i++){
-        if(tolower(n.operand[2]==arr[i]){found=true;break;}
+        if(tolower(n.operand[2])==arr[i]){found=true;break;}
 
        }
        if(found==false){n.errors[9]=true;}
     }
-else if(!strcasecmp(n.operation,"+RMO")){
+else if(n.operation=="+RMO"){
         pc+=4;
         n.memory=pc;
        n.errors[11]=true;
@@ -58,79 +58,79 @@ else if(!strcasecmp(n.operation,"+RMO")){
 
 ///
 
- else if(!strcasecmp(n.operation,"ADDR")){
+ else if(n.operation=="ADDR"){
         pc+=2;
         n.memory=pc;
-       if(n.operand.at(1)!=','){n.errors[9]=true;}
+       if(n.operand[1]!=','){n.errors[9]=true;}
        for(int i=0;i<6;i++){
-        if(tolower(n.operand.at(0))==arr[i]){found=true;break;}
+        if(tolower(n.operand[0])==arr[i]){found=true;break;}
 
        }
        if(found==false){n.errors[9]=true;}
        for(int i=0;i<6;i++){
-        if(tolower(n.operand.at(2))==arr[i]){found=true;break;}
+        if(tolower(n.operand[2])==arr[i]){found=true;break;}
 
        }
        if(found==false){n.errors[9]=true;}
     }
-else if(!strcasecmp(n.operation,"+ADDR")){
+else if((n.operation=="+ADDR")){
         pc+=4;
         n.memory=pc;
        n.errors[11]=true;
 
     }
 ///
-else if(!strcasecmp(n.operation,"SUBR")){
+else if(n.operation=="SUBR"){
         pc+=2;
         n.memory=pc;
-       if(n.operand.at(1)!=','){n.errors[9]=true;}
+       if(n.operand[1]!=','){n.errors[9]=true;}
        for(int i=0;i<6;i++){
-        if(tolower(n.operand.at(0))==arr[i]){found=true;break;}
+        if(tolower(n.operand[0])==arr[i]){found=true;break;}
 
        }
        if(found==false){n.errors[9]=true;}
        for(int i=0;i<6;i++){
-        if(tolower(n.operand.at(2))==arr[i]){found=true;break;}
+        if(tolower(n.operand[2])==arr[i]){found=true;break;}
 
        }
        if(found==false){n.errors[9]=true;}
     }
-else if(!strcasecmp(n.operation,"+SUBR")){
+else if(n.operation=="+SUBR"){
         pc+=4;
         n.memory=pc;
        n.errors[11]=true;
 
     }
 ///
-else if(!strcasecmp(n.operation,"COMPR")){
+else if((n.operation=="COMPR")){
         pc+=2;
         n.memory=pc;
-       if(n->operand.at(1)!=','){n.errors[9]=true;}
+       if(n.operand[1]!=','){n.errors[9]=true;}
        for(int i=0;i<6;i++){
-        if(tolower(n.operand.at(0))==arr[i]){found=true;break;}
+        if(tolower(n.operand[0])==arr[i]){found=true;break;}
 
        }
        if(found==false){n.errors[9]=true;}
        for(int i=0;i<6;i++){
-        if(tolower(n.operand.at(2))==arr[i]){found=true;break;}
+        if(tolower(n.operand[2])==arr[i]){found=true;break;}
 
        }
        if(found==false){n.errors[9]=true;}
     }
-else if(!strcasecmp(n.operation,"+COMPR")){
+else if((n.operation=="+COMPR")){
         pc+=4;
         n.memory=pc;
        n.errors[11]=true;
 
     }
 ///
-else if(!strcasecmp(n.operation,"TIXR")){
+else if((n.operation=="TIXR")){
         pc+=2;
         n.memory=pc;
-        if()
+      //  if()
 
     }
-else if(!strcasecmp(n.operation,"+TIXR")){
+else if(n.operation=="+TIXR"){
         pc+=4;
         n.memory=pc;
        n.errors[11]=true;
