ad2e8e139527ab743e029e08ea23ddc168e72956
Mayssara Omar <mayssaraomar@gmail.com>
2019-04-27 00:09:12 +0200

    add codeLine.cpp file

---
+++ sic-xe/sic-xe/codeLine.cpp
@@ -0,0 +1,26 @@
+#include "codeLine.h"
+codeLine::codeLine(string &line, int lineNo){
+  this->line = line;
+  this->lineNo = lineNo;
+}
+void codeLine::setMode(bool mode){
+  this->mode = mode;
+}
+void codeLine::loadPc(int pc){
+  this->pc = pc;
+}
+void codeLine::validate(){
+  if(mode == 0){ // free format
+    validateFreeFormat();
+  }
+  else { // fixed format
+    validateFixedFormat();
+  }
+}
+void codeLine::validateFreeFormat(){
+}
+void codeLine::validateFixedFormat(){
+} 
+int codeLine::getNewPc(){
+  return address;
+}
