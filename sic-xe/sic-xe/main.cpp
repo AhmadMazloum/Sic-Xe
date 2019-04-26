e3fccc57f2b1af02dbc9ac781c19ea26dfe5cfd3
Mayssara Omar <mayssaraomar@gmail.com>
2019-04-27 00:56:00 +0200

    Update main.cpp

--- sic-xe/sic-xe/main.cpp
+++ sic-xe/sic-xe/main.cpp
@@ -5,6 +5,7 @@ int main()
   char* readFilePath = "fileName.txt";
   char* writeFilePath = "fileName.txt";
   assembler *a = new assembler(0, readFilePath, writeFilePath);
+  a->run();
   cout <<"RUN SUCCESSFULLY";
   return 0;
 }
