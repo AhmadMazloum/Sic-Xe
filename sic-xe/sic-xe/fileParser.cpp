487c137bbddbc6b94d759e68c1881d97748aa213
Mayssara Omar <mayssaraomar@gmail.com>
2019-04-27 00:10:56 +0200

    add fileParser.cpp file

---
+++ sic-xe/sic-xe/fileParser.cpp
@@ -0,0 +1,8 @@
+#include "fileParser.h"
+
+vector<codeLine> fileParser::readFile(char*path){
+  vector<codeLine> codeLines;
+  return codeLines;
+}
+void fileParser::writeFile(char*path, vector<codeLine> &codeLines){ 
+}
