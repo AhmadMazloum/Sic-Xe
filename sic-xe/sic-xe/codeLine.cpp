d865f988958649f1b5fe8fdeca2f99bc102d2b67
Mayssara Omar <mayssaraomar@gmail.com>
2019-04-27 18:47:27 +0200

    Update codeLine.cpp

--- sic-xe/sic-xe/codeLine.cpp
+++ sic-xe/sic-xe/codeLine.cpp
@@ -10,17 +10,17 @@ void codeLine::setMode(bool mode){
 void codeLine::loadPc(int pc){
   this->pc = pc;
 }
-void codeLine::validate(){
+void codeLine::validate(unordered_set<string> &mustLabelled, unordered_set<string> &mustUnlabelled, map<string,regex> &operandPatterns, unordered_set<string> &labels, unordered_map<string,int> &unknownLabels){
   if(mode == 0){ // free format
-    validateFreeFormat();
+    validateFreeFormat(mustLabelled, mustUnlabelled, operandPatterns,labels, unknownLabels);
   }
   else { // fixed format
-    validateFixedFormat();
+    validateFixedFormat(mustLabelled, mustUnlabelled, operandPatterns,labels, unknownLabels);
   }
 }
-void codeLine::validateFreeFormat(){
+void codeLine::validateFreeFormat(unordered_set<string> &mustLabelled, unordered_set<string> &mustUnlabelled, map<string,regex> &operandPatterns, unordered_set<string> &labels, unordered_map<string,int> &unknownLabels){
 }
-void codeLine::validateFixedFormat(){
+void codeLine::validateFixedFormat(unordered_set<string> &mustLabelled, unordered_set<string> &mustUnlabelled, map<string,regex> &operandPatterns, unordered_set<string> &labels, unordered_map<string,int> &unknownLabels){
 } 
 int codeLine::getNewPc(){
   return address;
